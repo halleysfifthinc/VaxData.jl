b5881acef9d0b37ed581c456aef118b9afbdd620
Allen Hill <allenofthehills@gmail.com>
2018-06-10 17:40:48 -0400

    Make convertable types more explicit

--- src/vaxints.jl
+++ src/vaxints.jl
@@ -7,7 +7,7 @@ VaxInt16(x::UInt16) = reinterpret(VaxInt16, htol(x))
 VaxInt16(x::Signed) = reinterpret(VaxInt16, htol(trunc(Int16,x)))
 
 Base.convert(::Type{Int16},x::VaxInt16) = reinterpret(Int16,ltoh(x))
-Base.convert(::Type{T},x::VaxInt16) where T <: Union{Signed,AbstractFloat} = convert(T,convert(Int16,x))
+Base.convert(::Type{T},x::VaxInt16) where T <: Union{Int32,Int64,Int128,AbstractFloat} = convert(T,convert(Int16,x))
 
 Base.promote_rule(::Type{Int8},::Type{VaxInt16}) = Int16
 Base.promote_rule(::Type{T},::Type{VaxInt16}) where T <: Union{Int16,Int32,Int64,Int128,AbstractFloat} = T
@@ -16,7 +16,7 @@ VaxInt32(x::UInt32) = reinterpret(VaxInt32, htol(x))
 VaxInt32(x::Signed) = reinterpret(VaxInt32, htol(trunc(Int32,x)))
 
 Base.convert(::Type{Int32},x::VaxInt32) = reinterpret(Int32,ltoh(x))
-Base.convert(::Type{T},x::VaxInt32) where T <: Union{Signed,AbstractFloat} = convert(T,convert(Int32,x))
+Base.convert(::Type{T},x::VaxInt32) where T <: Union{Int16,Int64,Int128,AbstractFloat} = convert(T,convert(Int32,x))
 
 Base.promote_rule(::Type{T},::Type{VaxInt32}) where T <: Union{Int8,Int16} = Int32
 Base.promote_rule(::Type{T},::Type{VaxInt32}) where T <: Union{Int32,Int64,Int128,AbstractFloat} = T
