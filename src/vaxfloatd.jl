77627eb6998db872944ce89a7e865fffa73cb67c
Allen Hill <allenofthehills@gmail.com>
2018-08-03 14:17:38 -0400

    Fix bug that prevented small numbers from properly underflowing

--- src/vaxfloatd.jl
+++ src/vaxfloatd.jl
@@ -16,28 +16,35 @@ function VaxFloatD(x::T) where T <: Real
         ieeepart1 = part1
     end
 
+    e = reinterpret(Int64, ieeepart1 & IEEE_T_EXPONENT_MASK)
+
     if ieeepart1 & ~SIGN_BIT_64 === zero(UInt64)
-        vaxpart = zero(UInt64)
-    elseif (e = ieeepart1 & IEEE_T_EXPONENT_MASK) === IEEE_T_EXPONENT_MASK
+        # ±0.0 becomes 0.0
+        return zero(VaxFloatD)
+    elseif e === IEEE_T_EXPONENT_MASK
+        # Vax types don't support ±Inf or NaN
         throw(InexactError(:VaxFloatD, VaxFloatD, x))
     else
         e >>>= IEEE_T_MANTISSA_SIZE
         m = ieeepart1 & IEEE_T_MANTISSA_MASK
 
-        if e === zero(UInt64)
+        if e === zero(Int64)
             m = (m << 1) | (vaxpart2  >>> 31)
             vaxpart2 <<= 1
             while m & IEEE_T_HIDDEN_BIT === zero(UInt64)
                 m = (m << 1) | (vaxpart2  >>> 31)
                 vaxpart2 <<= 1
-                e -= 1
+                e -= one(Int64)
             end
             m &= IEEE_T_MANTISSA_MASK
         end
 
-        if (e += UNO64 + VAX_D_EXPONENT_BIAS - IEEE_T_EXPONENT_BIAS) <= 0
+        e += one(Int64) + VAX_D_EXPONENT_BIAS - IEEE_T_EXPONENT_BIAS
+        if e <= zero(Int64)
+            # Silent underflow
             return zero(VaxFloatD)
         elseif e > (2*VAX_D_EXPONENT_BIAS - 1)
+            # Overflow
             throw(InexactError(:VaxFloatD, VaxFloatD, x))
         else
             vaxpart = (ieeepart1 & SIGN_BIT_64) |
@@ -62,6 +69,14 @@ function VaxFloatD(x::T) where T <: Real
     return reinterpret(VaxFloatD, res)
 end
 
+Base.typemax(::Type{VaxFloatD}) = VaxFloatD(0xffffffffffff7fff)
+Base.floatmax(::Type{VaxFloatD}) = VaxFloatD(0xffffffffffff7fff)
+Base.typemin(::Type{VaxFloatD}) = VaxFloatD(0x0000000000000080)
+Base.floatmin(::Type{VaxFloatD}) = VaxFloatD(0x0000000000000080)
+
+Base.zero(::Type{VaxFloatD}) = VaxFloatD(0x0000000000000000)
+Base.one(::Type{VaxFloatD}) = VaxFloatD(0x0000000000004080)
+
 function Base.convert(::Type{Float64}, x::VaxFloatD)
     y = reinterpret(UInt64, ltoh(x))
 
@@ -73,11 +88,13 @@ function Base.convert(::Type{Float64}, x::VaxFloatD)
     vaxpart_4 = (y >>> 48) & bmask16
     vaxpart2 = (vaxpart_3 << 16) | vaxpart_4
 
-    if vaxpart1 & VAX_D_EXPONENT_MASK=== zero(UInt64)
+    if vaxpart1 & VAX_D_EXPONENT_MASK === zero(UInt64)
         if vaxpart1 & SIGN_BIT_64 === SIGN_BIT_64
+            # Reserved floating-point reserved operand
             throw(InexactError(:convert, Float64, x))
         end
 
+        # Dirty zero
         return zero(Float64)
     else
         ieeepart1 = ((vaxpart1 & SIGN_BIT_64) |
@@ -101,9 +118,6 @@ function Base.convert(::Type{Float64}, x::VaxFloatD)
     return reinterpret(Float64,res)
 end
 Base.convert(::Type{T},x::VaxFloatD) where T <: Union{Float16,Float32, BigFloat, Integer} = convert(T,convert(Float64,x))
-
-Base.zero(::Type{VaxFloatD}) = VaxFloatD(0x0000000000000000)
-Base.one(::Type{VaxFloatD}) = VaxFloatD(0x0000000000004080)
 
 Base.promote_rule(::Type{T},x::Type{VaxFloatD}) where T <: Union{AbstractVax, Float16, Float32, Float64, Integer} = Float64
 Base.promote_rule(::Type{BigFloat},x::Type{VaxFloatD}) = BigFloat
--- src/vaxfloatg.jl
+++ src/vaxfloatg.jl
@@ -17,28 +17,35 @@ function VaxFloatG(x::T) where T <: Real
         ieeepart1 = part1
     end
 
+    e = reinterpret(Int64, ieeepart1 & IEEE_T_EXPONENT_MASK)
+
     if ieeepart1 & ~SIGN_BIT_64 === zero(UInt64)
-        vaxpart = zero(UInt64)
-    elseif (e = ieeepart1 & IEEE_T_EXPONENT_MASK) === IEEE_T_EXPONENT_MASK
+        # ±0.0 becomes 0.0
+        return zero(VaxFloatG)
+    elseif e === IEEE_T_EXPONENT_MASK
+        # Vax types don't support ±Inf or NaN
         throw(InexactError(:VaxFloatG, VaxFloatG, x))
     else
         e >>>= VAX_G_MANTISSA_SIZE
         m = ieeepart1 & VAX_G_MANTISSA_MASK
 
-        if e === zero(UInt64)
+        if e === zero(Int64)
             m = (m << 1) | (vaxpart2 >>> 31)
             vaxpart2 <<= 1
             while m & VAX_G_HIDDEN_BIT === zero(UInt64)
                 m = (m << 1) | (vaxpart2 >>> 31)
                 vaxpart2 <<= 1
-                e -= one(UInt64)
+                e -= one(Int64)
             end
             m &= VAX_G_MANTISSA_MASK
         end
 
-        if (e += UNO64 + VAX_G_EXPONENT_BIAS - IEEE_T_EXPONENT_BIAS) <= 0
+        e += one(Int64) + VAX_G_EXPONENT_BIAS - IEEE_T_EXPONENT_BIAS
+        if e <= zero(Int64)
+            # Silent underflow
             return zero(VaxFloatG)
         elseif e > (2*VAX_G_EXPONENT_BIAS - 1)
+            # Overflow
             throw(InexactError(:VaxFloatG, VaxFloatG, x))
         else
             vaxpart = (ieeepart1 & SIGN_BIT_64) | (e << VAX_G_MANTISSA_SIZE) | m
@@ -59,6 +66,14 @@ function VaxFloatG(x::T) where T <: Real
     return reinterpret(VaxFloatG, res)
 end
 
+Base.typemax(::Type{VaxFloatG}) = VaxFloatG(0xffffffffffff7fff)
+Base.floatmax(::Type{VaxFloatG}) = VaxFloatG(0xffffffffffff7fff)
+Base.floatmin(::Type{VaxFloatG}) = VaxFloatG(0x0000000000000010)
+Base.typemin(::Type{VaxFloatG}) = VaxFloatG(0x0000000000000010)
+
+Base.zero(::Type{VaxFloatG}) = VaxFloatG(0x0000000000000000)
+Base.one(::Type{VaxFloatG}) = VaxFloatG(0x0000000000004010)
+
 function Base.convert(::Type{Float64}, x::VaxFloatG)
     y = reinterpret(UInt64, ltoh(x))
 
@@ -70,19 +85,25 @@ function Base.convert(::Type{Float64}, x::VaxFloatG)
     vaxpart_4 = (y >>> 48) & bmask16
     vaxpart2 = (vaxpart_3 << 16) | vaxpart_4
 
-    if (e = vaxpart1 & VAX_G_EXPONENT_MASK) === zero(UInt64)
+    e = reinterpret(Int64, vaxpart1 & VAX_G_EXPONENT_MASK)
+    if e === zero(Int64)
         if vaxpart1 & SIGN_BIT_64 === SIGN_BIT_64
+            # Reserved floating-point reserved operand
             throw(InexactError(:convert, Float64, x))
         end
 
+        # Dirty zero
+        println("dirty")
         return zero(Float64)
     else
         e >>>= VAX_G_MANTISSA_SIZE
 
-        if (e -= UNO64 + VAX_G_EXPONENT_BIAS - IEEE_T_EXPONENT_BIAS) > zero(UInt64)
+        e -= one(Int64) + VAX_G_EXPONENT_BIAS - IEEE_T_EXPONENT_BIAS
+        if e > zero(Int64)
             ieeepart1 = vaxpart1  - ((UNO64 + VAX_G_EXPONENT_BIAS - IEEE_T_EXPONENT_BIAS) << IEEE_T_MANTISSA_SIZE)
             ieeepart2 = vaxpart2
         else
+            # Subnormal result
             vaxpart1 = (vaxpart1 & (SIGN_BIT_64 | VAX_G_MANTISSA_MASK)) | VAX_G_HIDDEN_BIT
             ieeepart1 = (vaxpart1 & SIGN_BIT_64) | ((vaxpart1 & (VAX_G_HIDDEN_BIT | VAX_G_MANTISSA_MASK)) >>> (1 - e))
             ieeepart2 = (vaxpart1 << (31 + e)) | (vaxpart2 >>> (1 - e))
@@ -102,9 +123,6 @@ function Base.convert(::Type{Float64}, x::VaxFloatG)
     return reinterpret(Float64,res)
 end
 Base.convert(::Type{T},x::VaxFloatG) where T <: Union{Float16, Float32, BigFloat, Integer} = convert(T,convert(Float64,x))
-
-Base.zero(::Type{VaxFloatG}) = VaxFloatG(0x0000000000000000)
-Base.one(::Type{VaxFloatG}) = VaxFloatG(0x0000000000004010)
 
 Base.promote_rule(::Type{T},x::Type{VaxFloatG}) where T <: Union{AbstractVax, Float16, Float32, Float64, Integer} = Float64
 Base.promote_rule(::Type{BigFloat},x::Type{VaxFloatG}) = BigFloat
