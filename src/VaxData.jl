6237f25367baaf379e69f8b055a49a9cde2f1796
Allen Hill <allenofthehills@gmail.com>
2022-03-31 13:39:44 -0400

    Add support for some basic operations

--- src/VaxData.jl
+++ src/VaxData.jl
@@ -16,20 +16,12 @@ include("vaxfloatf.jl")
 include("vaxfloatd.jl")
 include("vaxfloatg.jl")
 include("promote.jl")
+include("math.jl")
 
 const VaxTypes = Union{VaxInt16,VaxInt32,VaxFloatF,VaxFloatD,VaxFloatG}
 
 function read(s::IO, ::Type{T}) where {T<:VaxTypes}
     return read!(s, Ref{T}(0))[]::T
-end
-
-# Define common arithmetic operations (default for two of the same unknown number type is to no-op error)
-# Promotion rules are such that the promotion will always be to a valid IEEE number type,
-# even in the case of two identical AbstractVax types
-for op in [:+, :-, :*, :/, :^, :<, :<=]
-    @eval(begin
-        Base.$op(x::T, y::T) where {T<:AbstractVax} = ($op)(promote(x,y)...)
-    end)
 end
 
 end # module
---
+++ src/math.jl
@@ -0,0 +1,71 @@
+# Define common arithmetic operations (default for two of the same unknown number type is to no-op error)
+# Promotion rules are such that the promotion will always be to a valid IEEE number type,
+# even in the case of two identical AbstractVax types
+for op in [:+, :-, :*, :/, :^, :<=]
+    @eval(begin
+        Base.$op(x::T, y::T) where {T<:AbstractVax} = ($op)(promote(x,y)...)
+    end)
+end
+
+Base.signbit(x::VaxFloat) = !iszero(x.x & 0x8000)
+Base.:-(x::T) where {T<:VaxFloat} = T(x.x ⊻ 0x8000)
+
+function Base.:<(x::T,y::T) where {T<:VaxFloat}
+    if signbit(x) == signbit(y)
+        return (swap16bword(x.x) & (typemax(uinttype(T)) >> 1)) < (swap16bword(y.x) & (typemax(uinttype(T)) >> 1))
+    else
+        return signbit(x) > signbit(y)
+    end
+end
+
+# copied and slightly modified from Base
+function Base.nextfloat(f::VaxFloat, d::Integer)
+    F = typeof(f)
+    fumax = swap16bword(typemax(f).x)
+    U = typeof(fumax)
+
+    fi = signed(swap16bword(f.x))
+    fneg = fi < 0
+    fu = unsigned(fi & typemax(fi))
+
+    dneg = d < 0
+
+    da = Base.uabs(d)
+    if da > typemax(U)
+        fneg = dneg
+        fu = fumax
+    else
+        du = da % U
+        if fneg ⊻ dneg
+            if du > fu
+                fu = min(fumax, du - fu)
+                fneg = !fneg
+            else
+                fu = fu - du
+            end
+        else
+            if fumax - fu < du
+                fu = fumax
+            else
+                fu = fu + du
+            end
+        end
+    end
+    if fneg
+        fu |= SIGN_BIT
+    end
+
+    # Jump past the VAX FP reserved operand (sign = 1, exp = 0, mant ≠ 0)
+    dz_hi = ~(exponent_mask(F) % U)
+    dz_lo = dz_hi - significand_mask(F)
+    if dz_lo ≤ fu ≤ dz_hi
+        return dneg ? nextfloat(F(0x00008080), d + 1) :
+                      nextfloat(zero(F), d - 1)
+    end
+
+    return F(swap16bword(fu))
+end
+
+Base.nextfloat(f::VaxFloat) = nextfloat(f,1)
+Base.prevfloat(f::VaxFloat) = nextfloat(f,-1)
+Base.prevfloat(f::VaxFloat, d::Integer) = nextfloat(f, -d)
